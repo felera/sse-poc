9da92f08115c242fd8173fa98165fe4aaac0e670
francisco <francisco@pop-os.localdomain>
2021-10-11 20:20:30 -0500

    change port to 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 //const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 let clients = [];
 let facts = [];
 
